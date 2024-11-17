f179f7c6691f55df8d0de6113c6eeb4f5d3f8f21
Liudmila <l7327833@gmail.com>
2024-11-17 20:56:05 +0100

    Update LAB1.cpp

--- LAB1.cpp
+++ LAB1.cpp
@@ -2,18 +2,15 @@
 #include <cmath>
 using namespace std;
 
-
-int main()
-{
-    double b=1.5, ha=0.1 ,E=0.001, hx= 0.21 ,z;
-    for(double x=0.5; x<=3.1; x+=hx)
-        for(double a=-0.1; a<=-1.4; a+= ha){
-            if(x<b )
-                z= sin(fabs(a*x+ pow(b,a)));
-            else{
-                z= cos(fabs(a*x- pow(b,a)));
-            }
+int main() {
+    double b = 1.5, ha = -0.1, E = 0.001, hx = 0.21, z;
+    for(double x = 0.5; x <= 3.1; x += hx) {
+        for(double a = -0.1; a >= -1.4; a += ha) {
+            if (x < b)
+                z = sin(fabs(a * x + pow(b, a)));
+            else
+                z = cos(fabs(a * x - pow(b, a)));
             cout << "z=\t" << z << endl;
         }
-    
+    }
 }
