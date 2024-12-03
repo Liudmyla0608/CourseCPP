15aec5b4ed5eab35bd27d542fd543d372fd45532
Liudmila <l7327833@gmail.com>
2024-12-03 12:10:29 +0100

    Update main.cpp

--- LAB8/main.cpp
+++ LAB8/main.cpp
@@ -20,18 +20,22 @@
 using namespace std;
 
 int main() {
+    // Створюємо перший кінотеатр
     Cinema* x1;
-    string films1[] = {"Pretty Girl", "Human-Spider"};
     x1 = new Cinema("Silver", "New York", "Yolt Street", 22, 1997, 10);
 
+    // Додаємо фільми до першого кінотеатру
     x1->addFilm(Film("Pretty Girl", 2020, "Romance", 16, 120));
     x1->addFilm(Film("Human-Spider", 2002, "Action", 13, 121));
 
+    // Виводимо інформацію про перший кінотеатр
     cout << "Name of cinema 1: " << x1->get_name() << endl;
     cout << "Address of cinema 1: " << x1->get_address() << endl;
 
+    // Виводимо процент дитячих фільмів
     cout << "Percentage of kids films in cinema 1: " << x1->get_percentage_of_kids_films() << "%" << endl;
 
+    // Знаходимо найдовший фільм
     if (x1->get_average_duration() > 0) {
         Film longestFilm = x1->get_longest_film();
         cout << "Longest film in cinema 1: " << longestFilm.getTitle() << ", " << longestFilm.getDuration() << " minutes." << endl;
@@ -39,19 +43,27 @@ int main() {
         cout << "No films available to find the longest one." << endl;
     }
 
+    // Виводимо середню тривалість фільмів
     cout << "Average film duration in cinema 1: " << x1->get_average_duration() << " minutes." << endl;
 
+    // Видаляємо один із фільмів
     x1->remove_film("Human-Spider");
 
+    // Звільняємо пам'ять
     delete x1;
 
+    // Створюємо другий кінотеатр
     Cinema x2("Gold", "Los Angeles", "Yolt Street", 25, 1998, 5);
     x2.addFilm(Film("Toy Story", 1995, "Animation", 6, 81));
     x2.addFilm(Film("Frozen", 2013, "Animation", 0, 102));
 
+    // Виводимо інформацію про другий кінотеатр
     cout << "Name of cinema 2: " << x2.get_name() << endl;
     cout << "Address of cinema 2: " << x2.get_address() << endl;
     cout << "Percentage of kids films in cinema 2: " << x2.get_percentage_of_kids_films() << "%" << endl;
+
+    // Середня тривалість фільмів другого кінотеатру
+    cout << "Average film duration in cinema 2: " << x2.get_average_duration() << " minutes." << endl;
 
     return 0;
 }
